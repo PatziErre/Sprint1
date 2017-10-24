d1485129942c909eb825269f32ed875e3fe6fb40
PatziErre <patriciainksg@gmail.com>
2017-10-23 21:10:31 -0500

    Subiendo ejericio resuelto

--- ej6CajeroAutomatico/app.js
+++ ej6CajeroAutomatico/app.js
@@ -1,22 +1,40 @@
-// // Crea un programa que un cajero automático pueda 
-// utilizar para determinar el número mínimo de billetes 
-// a entregar dado un monto solicitado. El cajero empieza con:
-
-var dinero = {
-    billetes 100 : 100,
-    billetes 50 : 50,
-    billetes 20 : 200,
-    billetes 10 : 300,
-    billetes 5 : 500,
-    billetes 1 : 800
-};
-
-function calcularBilletes (montoSolicitado){
- var totalDeBilletes = 0
- var tiposDeBilletes = {}
- for (i=0 ; i <= dinero.length ; i++ ){
-    git add .
-    }
-}
-
-
+var billetes = {
+    "100": 100,
+    "50": 50,
+    "20": 200,
+    "10": 300,
+    "5": 500,
+    "1": 800,
+  }
+  
+  // Necesitamos sacar las llaves del objeto para ordenarlas
+  billetesKeys = Object.keys(billetes);
+  
+  // Aplicamos el sort para ordenar las llaves descendientemente
+  billetesKeys.sort(function(a,b){
+      return b - a
+  });
+  
+  function calcularBilletes(montoSolicitado) {
+    // Agregas una variable en donde vas a tener el total de billetes a darle
+    var totalDeBilletes = 0;
+    var montoPendiente = montoSolicitado;
+    // Comienzas un for para recorrer todos los tipos de billetes en el arreglo
+    for (i = 0; i < billetesKeys.length; i++) {
+      // Checas si aplica para cada tipo de billete
+      key = parseInt(billetesKeys[i]);
+      // Se usarian billetes si es que el monto pendiente dividido entre
+      var billetesUsados = Math.floor(montoPendiente/key);
+      
+      if (montoPendiente>= key && billetesUsados > 0){
+        montoPendiente = montoPendiente % billetes[key];
+        billetes[key] = billetes[key] - billetesUsados;
+        console.log("Se usará " + billetesUsados + " billete de denominación $" + key); 
+        totalDeBilletes = totalDeBilletes + billetesUsados;
+      }
+     }
+    console.log("Monto solicitado $" + montoSolicitado + " se entregan " + totalDeBilletes + " billetes");
+    console.log("En el cajero quedan: ", billetes);
+  }
+  
+  calcularBilletes(150);
